dd91bc779a4de68173eb47a6ebe80ea74d7f1e9f
wilsonge <georgejameswilson@googlemail.com>
2013-06-16 13:46:22 +0100

    Large Rewrite
    See change log for more info

--- mod_accordion/tmpl/default.php
+++ mod_accordion/tmpl/default.php
@@ -1,73 +1,94 @@
 <?php 
 /**
-* @package		JJ Accordion
-* @author		JoomJunk
-* @copyright	Copyright (C) 2011 - 2012 JoomJunk. All Rights Reserved
-* @license		http://www.gnu.org/licenses/gpl-3.0.html
-*/
+ * @package    JJ_Accordion
+ * @author     JoomJunk <admin@joomjunk.co.uk>
+ * @copyright  Copyright (C) 2011 - 2012 JoomJunk. All Rights Reserved
+ * @license    GNU General Public License version 3; http://www.gnu.org/licenses/gpl-3.0.html
+ */
 
-// no direct access
-defined('_JEXEC') or die('Restricted access'); 
+// No direct access
+defined('_JEXEC') or die('Restricted access');
 
 ?>
 <div class="accordion" id="accordion">
-	<?php for ($i=0; $i < $items; $i++) : ?>
+	<?php
+	for ($i = 0; $i < $items; $i++)
+	{
+	?>
 			<div>
 				<?php
 				$listitem = $list[$i];
 				$item_class = "item" . ($i + 1);
-				if ($i == 0) $item_class .= " first";
-				if ($i == $items - 1) $item_class .= " last";
+
+				if ($i == 0)
+				{
+					$item_class .= " first";
+				}
+
+				if ($i == $items - 1)
+				{
+					$item_class .= " last";
+				}
 				?>
-				
+
 				<h3 class="jjheader">
 					<?php echo $listitem->title ?>
 				</h3>
-				
+
 				<div class="accordion-section">
 					<div class="accordion-content">
-						<?php 		
-						if(version_compare(JVERSION,'3.0','ge')) {
-								$item = modAccordionHelper::renderItem30($listitem, $params, $access);
-							if ($params->get('readmore')) {
-								modAccordionHelper::truncate($item->introtext, $params->get('textlimit', 25), $ending = '...', true, true);
-							}						
+						<?php
+							if ($params->get('image') && !empty($listitem->images))
+							{
+								$images = json_decode($listitem->images);
 
-							echo $item->introtext;
-							
-							if (isset($item->link) && $item->readmore != 0 && $params->get('readmore')) :
-								echo '<a class="readmore btn btn-small btn-primary" href="'.$item->link.'">'.$item->linkText.'</a>';
-							endif;
-						} 
-						else {
-							if ($params->get('readmore')) {
-								modAccordionHelper::truncate($item->introtext, $params->get('textlimit', 25), $ending = '...', true, true);
-							}
-						
-							echo $listitem->introtext;
-						
-							if (isset($listitem->link) && $params->get('readmore')) {
-								if ($params->get('jj_style') == 'bootstrap') {
-									echo '<br /><a class="readmore btn btn-small btn-primary" href="'.$listitem->link.'">' . JText::_('MOD_ACCORDION_READ_MORE') . '</a>';
+								if (!empty($images->image_intro) && !empty($images->image_intro_alt))
+								{
+									echo '<image src="' . $images->image_intro . '" alt="' . $images->image_intro_alt . '" height="20px" />';
 								}
-							else {
-								echo '<br /><a class="readmore" href="'.$listitem->link.'">' . JText::_('MOD_ACCORDION_READ_MORE') . '</a>';
 							}
+
+							if ($params->get('readmore'))
+							{
+								$listitem->introtext = ModAccordionHelper::truncate($listitem->introtext, $params->get('textlimit', 25), $ending = '...', false, true, false);
 							}
-						}
+
+							echo $listitem->introtext;
+
+							if (isset($listitem->link) && $params->get('readmore'))
+							{
+								echo '<br /><a class="readmore btn btn-small btn-primary" href="' . $listitem->link . '">' . JText::_('MOD_ACCORDION_READ_MORE') . '</a>';
+							}
 						?>
 					</div>
 				</div>
 			</div>
-	<?php endfor; ?>
+	<?php
+	}
+	?>
 </div>
 
 <?php
-if($params->get('open', '1')) { $open=0;} else {$open=1-2;}
-if($params->get('multi', '1')) { $multi=0;} else {$multi=1;}
+if ($params->get('open', '1'))
+{
+	$open = 0;
+}
+else
+{
+	$open = 1 - 2;
+}
+
+if ($params->get('multi', '1'))
+{
+	$multi = 0;
+}
+else
+{
+	$multi = 1;
+}
 ?>
 
 <script type="text/javascript">
-	var parentAccordion=new JJ.accordion.slider('parentAccordion'); 
+	var parentAccordion=new JJ.accordion.slider('parentAccordion');
 	parentAccordion.init('accordion','h3',<?php echo $multi; ?>,<?php echo $open; ?>,'jjheader');
 </script>
