7eaec9bfea6adb1f2374e42502e3097783703c7b
Charlie <lodder15@hotmail.co.uk>
2014-08-07 19:03:01 +0100

    removed CSS references

--- modules/mod_accordion/mod_accordion.php
+++ modules/mod_accordion/mod_accordion.php
@@ -18,6 +18,7 @@ $access->canPublish = 0;
 
 $list = ModAccordionHelper::getList($params);
 JHtml::_('jquery.framework');
+JHtml::_('stylesheet', 'mod_accordion/css/accordion.css', array(), true);
 
 $items = count($list);
 
@@ -26,35 +27,6 @@ if (!$items)
 	echo JText::_('MOD_ACCORDION_NO_ARTICLES');
 
 	return;
-}
-
-$jj_style = $params->get('jj_style', 'light');
-
-$document = JFactory::getDocument();
-
-if ($params->get('arrow', '1'))
-{
-	$arrow = '.jjaccordion .accordion-toggle:after { '
-		. 'background: url(' . JUri::root() . 'media/mod_accordion/arrow-right.png) no-repeat;'
-		. 'display: block;'
-		. 'height: 15px; '
-		. 'width: 15px; '
-		. 'float: left; '
-		. 'padding-right: 10px;'
-		. '}'
-		. '.jjaccordion .accordion-toggle collapsed:after { '
-		. 'background: url(' . JUri::root() . 'media/mod_accordion/arrow-down.png) no-repeat;'
-		. '}';
-	$document->addStyleDeclaration($arrow);
-}
-
-switch ($jj_style)
-{
-	case "dark":
-		JHtml::_('stylesheet', 'mod_accordion/css/accordion-dark.css', array(), true);
-		break;
-	default:
-		// nothing here
 }
 
 $acc_id = $module->id;
