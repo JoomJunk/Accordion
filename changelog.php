5e37aeaee86d1408b5aa8320e4f1a499164bb890
George Wilson <georgejameswilson@googlemail.com>
2014-08-10 15:44:12 +0100

    Update changelog

--- changelog.php
+++ changelog.php
@@ -15,6 +15,10 @@ $ -> Language fix or change
 - -> Removed
 ! -> Note
 
+Version 3.0.2
+# Fix error in updating accordion
+# Fix accordion breaking when there's multiple instances on a page
+
 Version 3.0.1
 # Fixed bug in install script
 
