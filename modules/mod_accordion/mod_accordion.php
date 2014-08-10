4a6d59c5b568782349f7f56db8a086283819fa2a
wilsonge <georgejameswilson@googlemail.com>
2014-08-10 14:18:54 +0100

    Add bootstrap CDN

--- modules/mod_accordion/mod_accordion.php
+++ modules/mod_accordion/mod_accordion.php
@@ -20,6 +20,15 @@ $list = ModAccordionHelper::getList($params);
 JHtml::_('jquery.framework');
 JHtml::_('stylesheet', 'mod_accordion/css/accordion.css', array(), true);
 
+// Some stupid template devs stop people being able to use Joomla's bootstrap.
+// This is a last minute fallback option to allow it to still be used for the accordion.
+if ($params->get('bootstrap', 0))
+{
+	$doc = JFactory::getDocument();
+	$doc->addStyleSheet('http://maxcdn.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css');
+	$doc->addScript('http://maxcdn.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js');
+}
+
 $items = count($list);
 
 if (!$items)
