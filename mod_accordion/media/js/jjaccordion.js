259064e1b88b1b495a45cb074a63faf814d3683e
George Wilson <georgejameswilson@googlemail.com>
2014-12-29 01:09:10 +0000

    Add some code comments

--- mod_accordion/media/js/jjaccordion.js
+++ mod_accordion/media/js/jjaccordion.js
@@ -11,26 +11,34 @@
 			open: true,
 			speed: 'fast'
 		};
-		
+
+		// Merge defaults with injected params
 		var options 	= $.extend({}, defaults, options);
+
+		// The headers for our accordion instance
 		var jjaccordion = $('#accordion' + options.id + ' .' + options.header);
 
+		// Check we have an id to ensure we are triggering on the right element
 		if (!options.id) 
 		{
 			throw "Must provide an id.";
 		}
 
+		// Ensure all elements are closed on page load
 		jjaccordion.toggleClass(options.inactiveheader);
 
+		// But open the first accordion pane if instructed to do so
 		if (open)
 		{
 			jjaccordion.first().toggleClass(options.activeheader).toggleClass(options.inactiveheader);
 			$('#accordion' + options.id + ' .' + options.content).first().slideDown(options.speed).toggleClass(options.opencontent);
 		}
 
+		// On clicking on an element open that pane
 		jjaccordion.on('click', function () {
 			var self = $(this);
 
+			// If inactive open the pane and if currently active close the pane.
 			if(self.hasClass(options.inactiveheader)) 
 			{
 				$('#accordion' + options.id + ' .' + options.activeheader).toggleClass(options.activeheader).toggleClass(options.inactiveheader).next().slideToggle(options.speed).toggleClass(options.opencontent);
@@ -43,7 +51,6 @@
 		});
 
 		return false;
-
 	};
 	
 })(jQuery);
